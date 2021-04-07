b9ae120e02d7b696e4fa2b76cf54fa45bf0aa148
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-04-07 15:47:53 +0200

    messaggio cambiato

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,6 @@ var nome = prompt('Inserisci il tuo nome: ');
 var cognome = prompt('Inserisci il tuo cognome: ');
 var colore = prompt('Inserisci il tuo colore preferito: ');
 
-document.getElementById('text').innerHTML = 'Ecco i dati inseriti: ' + nome + cognome + colore + '21';
+document.getElementById('text').innerHTML = 'Ecco la tua password generata: ' + nome + cognome + colore + '21';
 
 alert('Password Debolissima')
